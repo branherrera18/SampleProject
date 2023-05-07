a2905f7721d07a3ad074cb9ee705f506d0e9b264
Brandon Herrera Cerdas <brandonherrera011@hotmail.com>
2023-05-07 11:18:17 -0600

    using ServletConfig

--- src/main/java/com/test/servlets/GetServlet.java
+++ src/main/java/com/test/servlets/GetServlet.java
@@ -3,17 +3,24 @@ package com.test.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import jdk.internal.org.jline.reader.PrintAboveWriter;
 
-
+@WebServlet(urlPatterns = "/getServlet",
+            initParams = @WebInitParam(name="URL", value="http://www.weatherservice.com"))
 public class GetServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        ServletConfig config = getServletConfig();
+        System.out.println(config.getInitParameter("URL"));
         String htmlResponse = "<html><h3>Welcome to Servlets!</h3></html>";
         PrintWriter writer = resp.getWriter();
         writer.write(htmlResponse);
